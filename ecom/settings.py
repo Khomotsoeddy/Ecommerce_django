c1723effd7de8759c704f1b08447e4f30fcf5c8a
Khomotso <khomotsoeddy@gmail.com>
2023-03-13 12:48:17 +0200

    change database

--- ecom/settings.py
+++ ecom/settings.py
@@ -83,18 +83,18 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default':dj_database_url.config(
-        default='postgres://mega_db_user:wGmKdZFlAY3TlLngLCf5CWEcri94ubA3@dpg-cg7do702qv28u2siklqg-a.oregon-postgres.render.com/mega_db',
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default':dj_database_url.config(
+#         default='postgres://mega_db_user:wGmKdZFlAY3TlLngLCf5CWEcri94ubA3@dpg-cg7do702qv28u2siklqg-a.oregon-postgres.render.com/mega_db',
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
